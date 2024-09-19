d1b920c015327a2a52ea695b88c49283817bdc64
Valéria Cristina dos Santos Silva <CIANDT\valeria.silva@lnb034968cps.ciandt.global>
2024-09-19 15:58:24 -0300

    Codigo para salvar dado no bucket

--- airflow/dags/util/ingestion.py
+++ airflow/dags/util/ingestion.py
@@ -1,9 +1,10 @@
 import yaml
-from pyspark.sql import SparkSession,DataFrame
+import os
+import requests
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import col
 import logging
-import requests
-import os
+from .storage import Storage  # Certifique-se de que o arquivo storage.py está no mesmo diretório ou no PYTHONPATH
 
 class IngestionPipeline:
     """Pipeline principal para ingestão de dados."""
@@ -18,9 +19,20 @@ class IngestionPipeline:
         # Configurar Spark
         self.spark = self.create_spark_session(self.spark_config)
 
-        # Configurar bucket e caminho para DataProcessor
-        self.bucket = self.ingestion_config.get('bucket', '')
+        # Configurar buckets
+        self.bucket_bronze = self.parquet_config.get('bucket_bronze', '')
+        self.bucket_silver = self.parquet_config.get('bucket_silver', '')
+        self.bucket_gold = self.parquet_config.get('bucket_gold', '')
+
         self.path_template = self.ingestion_config.get('path', '')
+
+        # Configurar Storage
+        self.storage = Storage(
+            endpoint_url=self.spark_config['s3a']['endpoint'],
+            access_key=self.spark_config['s3a']['access_key'],
+            secret_key=self.spark_config['s3a']['secret_key'],
+            bucket=''  # Inicialmente vazio, será definido conforme necessário
+        )
 
         self.logger = logging.getLogger(__name__)
         
@@ -32,7 +44,6 @@ class IngestionPipeline:
     
     @staticmethod
     def create_spark_session(config: dict) -> SparkSession:
-
         """Cria uma sessão Spark configurada."""
         return SparkSession.builder \
             .appName(config.get('app_name', 'DefaultApp')) \
@@ -40,45 +51,61 @@ class IngestionPipeline:
             .config("spark.hadoop.fs.s3a.secret.key", config.get('s3a', {}).get('secret_key', '')) \
             .config("spark.hadoop.fs.s3a.endpoint", config.get('s3a', {}).get('endpoint', '')) \
             .config("spark.hadoop.fs.s3a.path.style.access", config.get('s3a', {}).get('path_style_access', '')) \
+            .config("spark.hadoop.fs.s3a.path.style.access", "true") \
             .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.2,com.amazonaws:aws-java-sdk-bundle:1.11.1000") \
             .getOrCreate()
     
-    @staticmethod
-    def download_file(url: str, local_path: str):
+    def download_file(self, url: str, local_path: str):
         """Baixa o arquivo de uma URL e salva localmente."""
+        self.logger.info(f"Realiza request na URL {url}")
         response = requests.get(url)
         response.raise_for_status()  # Levanta um erro se o download falhar
         with open(local_path, 'wb') as file:
             file.write(response.content)
+            self.logger.info(f"Arquivo gravado em {local_path}")
+
+
+    def download_file_bucket(self, s3_path: str, local_path: str, bucket_name: str):
+        """Baixa um arquivo do bucket S3 especificado para o caminho local."""
+        self.logger.info(f"Baixa um arquivo do bucket S3 para o caminho local especificado")
+        self.storage.set_bucket(bucket_name)
+        self.storage.download_file(s3_path, local_path)
     
+    def upload_file_bucket(self, local_path:str, bucket_name: str, s3_path:str, year: int, month: int ):
+        """Baixa o arquivo de uma URL e salva no bucket S3 especificado."""
+        month_str = f'{month:02d}'
+        s3_path = f'yellow_tripdata_{year}-{month_str}.parquet'
+        parquet_path = f"s3a://{self.bucket_silver}/year={year}/month={month_str}/yellow_tripdata.parquet"
+
+        self.storage.set_bucket(bucket_name)
+        self.storage.upload_file_bucket(local_path,bucket_name,s3_path, year,month)
+
+        
     def process_file(self, year: int, month: int):
         self.logger.info("Processa o arquivo de um mês específico e salva no MinIO.")
         month_str = f"{month:02d}"
         
-        # URL do arquivo Parquet e caminho local para salvar o arquivo
+        # URL do arquivo Parquet 
         file_url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month_str}.parquet'
-        local_file_path = f'/tmp/yellow_tripdata_{year}-{month_str}.parquet'
-        #local_file_path = f's3a://bronze/yellow_tripdata_{year}-{month_str}.parquet'
+        local_path = f'/opt/airflow/tmp/yellow_tripdata_{year}-{month_str}.parquet'
+        local_file_path  = f'/opt/airflow/tmp/yellow_tripdata_{year}-{month_str}.parquet'
 
-        
         # Baixa o arquivo
-        self.logger.info(f"****** Baixando arquivo: {file_url} *******")
+        #self.logger.info(f"****** Baixando arquivo: {file_url} *******")
+        #s3_path = f'yellow_tripdata_{year}-{month_str}.parquet'
+        #self.upload_file_bucket(file_url, year, month, self.bucket_bronze)
+
         self.download_file(file_url, local_file_path)
 
-
-        if not os.path.isfile(local_file_path):
-            self.logger.error(f"Erro: Arquivo {local_file_path} não foi encontrado.")
-            return
-
-        self.logger.info(f"****** Finalizou download em {local_file_path}*******")
-
+        # Lê o arquivo no Spark
         try:
             df: DataFrame = self.spark.read.parquet(local_file_path)
-            self.logger.info(f"df: {df.show(2)} *******")
+            self.logger.info(f"DataFrame lido com sucesso")
         except Exception as e:
             self.logger.error(f"Erro ao ler o arquivo Parquet: {e}")
             return
 
+        # Processa o DataFrame
         df_filtered = df.select(
             col("VendorID"),
             col("passenger_count"),
@@ -86,13 +113,16 @@ class IngestionPipeline:
             col("tpep_pickup_datetime"),
             col("tpep_dropoff_datetime")
         )
+
+        self.logger.info(f"Arquivo filtrado {df_filtered.columns}")
         # Define o caminho dinâmico para salvar no MinIO
-        parquet_path = f"s3a://{self.bucket}/year={year}/month={month_str}/yellow_tripdata.parquet"
+        parquet_path = f"s3a://{self.bucket_silver}/year={year}/month={month_str}/yellow_tripdata.parquet"
 
         # Salva o DataFrame como Parquet no MinIO
-        df_filtered.write.parquet(parquet_path, mode="overwrite")
-        self.logger.info(f"******** Arquivo Parquet {year}-{month_str} salvo com sucesso no MinIO {parquet_path}! *******")
 
+        self.upload_file_bucket(local_path=local_path,bucket_name=self.bucket_silver,s3_path=parquet_path, year=year,month =month)
+        #df_filtered.write.parquet(parquet_path, mode="overwrite")
+        self.logger.info(f"******** Arquivo Parquet {year}-{month_str} salvo com sucesso no MinIO {parquet_path}! *******")
 
     def create_hive_table(self, sql_file_path: str):
         """Cria a tabela no Hive usando um arquivo SQL."""
@@ -111,5 +141,5 @@ class IngestionPipeline:
 
 # Execução do pipeline
 #if __name__ == "__main__":
-  #  pipeline = IngestionPipeline('config/settings.yaml')
- #   pipeline.run()
+#    pipeline = IngestionPipeline('config/settings.yaml')
+#    pipeline.run()
--- airflow/dags/util/storage.py
+++ airflow/dags/util/storage.py
@@ -1,42 +1,68 @@
-import json
-import logging
-import os
-from urllib.parse import urlparse
-
 import boto3
-
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
+import requests
+import logging
 
 class Storage:
-    def __init__(self):
-        self.s3_client = boto3.client(
+    def __init__(self, endpoint_url: str, access_key: str, secret_key: str, bucket: str):
+        self.logger = logging.getLogger(__name__)
+        self.s3 = boto3.client(
             's3',
-            region_name='us-east-1',
-            endpoint_url=os.environ.get('MINIO_ENDPOINT'),
-            aws_access_key_id=os.environ.get('MINIO_ACCESS_KEY'),
-            aws_secret_access_key=os.environ.get('MINIO_SECRET_KEY')
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
         )
+        self.bucket = bucket
 
-    def __decompose_s3_path(self, s3_path: str) -> tuple:
-        parsed_url = urlparse(s3_path)
-        bucket = parsed_url.netloc
-        key = parsed_url.path.lstrip('/')
-        return bucket, key
+    def set_bucket(self, bucket: str):
+        """Define o bucket a ser usado nas operações."""
+        self.bucket = bucket
+        self.logger.info(f"bucket name: {self.bucket}")
 
-    def save_file_to_bucket(self, data: dict, path: str, file_type: str = 'json'):
-            """Salva arquivos no bucket, podendo ser JSON ou outros formatos."""
-            logging.info('Saving file to bucket...')
-            
-            # Definir o nome do arquivo com base no tipo
-            filename = f'file.{file_type}'
-            path_with_filename = path + filename
-            
-            # Extrair bucket e key
-            bucket, key = self.__decompose_s3_path(s3_path=path_with_filename)
-            
-            # Verificar tipo do arquivo e salvar apropriadamente
-            if file_type == 'json':
-                self.s3_client.put_object(Bucket=bucket, Key=key, Body=json.dumps(data))
-            else:
-                self.s3_client.put_object(Bucket=bucket, Key=key, Body=data)
-            
-            logging.info('File successfully saved!')
+    def download_file(self, s3_path: str, local_path: str):
+        """Baixa um arquivo do bucket S3 para o caminho local especificado."""
+        try:
+            self.s3.download_file(Bucket=self.bucket, Key=s3_path, Filename=local_path)
+            self.logger.info(f"Arquivo salvo em {local_path}")
+        except NoCredentialsError:
+            self.logger.error("Credenciais não encontradas para acessar o S3")
+        except ClientError as e:
+            self.logger.error(f"Erro ao baixar o arquivo do S3: {e}")
+        except Exception as e:
+            self.logger.error(f"Erro inesperado: {e}")
+
+    def upload_file(self, url: str, s3_path: str):
+        """Faz o upload de um arquivo baixado de uma URL para o bucket S3."""
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Verifica se o download foi bem-sucedido
+        except requests.exceptions.HTTPError as err:
+            self.logger.error(f"Erro ao fazer download do arquivo: {err}")
+            return
+
+        try:
+            self.s3.put_object(Bucket=self.bucket, Key=s3_path, Body=response.content)
+            self.logger.info(f"Arquivo salvo em s3://{self.bucket}/{s3_path}")
+        except NoCredentialsError:
+            self.logger.error("Credenciais não encontradas para acessar o S3")
+        except ClientError as e:
+            self.logger.error(f"Erro ao salvar o arquivo no S3: {e}")
+        except Exception as e:
+            self.logger.error(f"Erro inesperado: {e}")
+
+    def upload_file_bucket(self, local_path: str, bucket_name: str, s3_path: str, year: int, month: int):
+        """Baixa o arquivo de uma URL e faz upload para o bucket S3 especificado."""
+        month_str = f'{month:02d}'  # Formata o mês com dois dígitos
+        # Define o caminho correto no S3
+        s3_path = f's3a://{bucket_name}/year={year}/month={month_str}/yellow_tripdata.parquet'
+        
+        try:
+            self.set_bucket(bucket_name)  # Define o bucket para o upload
+            self.s3.upload_file(Filename=local_path, Bucket=bucket_name, Key=s3_path.replace(f's3a://{bucket_name}/', ''))
+            self.logger.info(f"Arquivo carregado com sucesso para {s3_path}")
+        except NoCredentialsError:
+            self.logger.error("Credenciais não encontradas para acessar o S3")
+        except ClientError as e:
+            self.logger.error(f"Erro ao fazer upload do arquivo para o S3: {e}")
+        except Exception as e:
+            self.logger.error(f"Erro inesperado: {e}")
